694a96a4f7f84d4f1ac49017ca725e407ab7d48a
avroumi <avroumi97@gmail.com>
2026-07-30 12:26:40 +0300

    add invalid test , need to correct here later

---
+++ src/test/service/benefitsService.test.js
@@ -0,0 +1,25 @@
+import { assert } from "node:console";
+im;
+import service from "../../service/benefitsService.js";
+import { mock, describe, it, beforeEach } from "node:test";
+
+const mockData = {
+  benefitType: "giftCard",
+  details: { cardProvider: "jfh", monthlyValue: 7, validMerchants: ["hvs"] },
+  decisionReason: "fghf",
+  budgetApprived: false,
+};
+
+describe("test getBeneficeById", () => {
+  mock.method(service, "getBenefitsByIdService", () => ({
+    benefitType: "giftCard",
+    details: { cardProvider: "jfh", monthlyValue: 7, validMerchants: ["hvs"] },
+    decisionReason: "fghf",
+    budgetApprived: false,
+  }));
+  mock.fn(function getBenefitsById() {
+    null;
+  });
+  const benefit = getBenefitsByIdService(1);
+  assert.rejects(() => benefit);
+});
